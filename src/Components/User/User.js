251baaced46eecece0ac927f1ae8771789cac75e
Varun Muthu <varun_muthu@thbs.india.com>
2020-02-07 12:50:32 +0530

    updated for Conflicts

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -6,14 +6,10 @@ import { Route } from 'react-router-dom';
 class User extends React.Component {
     render() {
         return(
-<<<<<<< HEAD
-                <Navbar />
-=======
             <section>
                 <Navbar />
                 <h1>Users Page</h1>
             </section>
->>>>>>> e8c92cdbacb72bb8b2b813658ad4ef9a1aa45188
         )
     }
 }
